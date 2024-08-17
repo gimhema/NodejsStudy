5329091d686693340f851c8a1799d6b5ea0d916b
Kimmo <49871739+gimhema@users.noreply.github.com>
2024-08-17 10:33:50 +0900

    Update app.js

--- ws-chat/app.js
+++ ws-chat/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { join } = require('node:path');
 
 const app = express();
 const server = createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.sendFile(join(__dirname, 'index.html'));
+//     res.send('<h1>Hello World</h1>');
 });
 
 server.listen(3000, () => {
