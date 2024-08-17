1ce197b8400aad895a93fb01084766603d36b57d
Kimmo <49871739+gimhema@users.noreply.github.com>
2024-08-17 10:45:31 +0900

    Update app.js

--- ws-chat/app.js
+++ ws-chat/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
-//     res.send('<h1>Hello World</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message : ' + msg);
+    });
 });
 
 server.listen(3000, () => {
