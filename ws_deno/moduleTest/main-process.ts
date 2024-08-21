4ff2a702d282030efdb187f8a50ba131018976ed
Kimmo <49871739+gimhema@users.noreply.github.com>
2024-08-21 02:00:12 -0400

    module test

---
+++ ws_deno/moduleTest/main-process.ts
@@ -0,0 +1,8 @@
+import { SubFunction1, SubFunction2 } from "./submodule/sub-process.ts";
+
+
+SubFunction1()
+
+SubFunction2()
+
+
---
+++ ws_deno/moduleTest/submodule/sub-process.ts
@@ -0,0 +1,11 @@
+
+
+
+export function SubFunction1() {
+    console.log("Call Sub Functuon 1");
+}
+
+export function SubFunction2() {
+    console.log("Call Sub Functuon 2");
+}
+
