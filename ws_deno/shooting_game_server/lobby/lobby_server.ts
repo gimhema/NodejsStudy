24f53a2127680d1f05695a4f86445ecea5c2e387
kimmo <hemakim0215@naver.com>
2024-09-24 00:15:52 +0900

    .

--- ws_deno/shooting_game_server/lobby/lobby_server.ts
+++ ws_deno/shooting_game_server/lobby/lobby_server.ts
@@ -1,30 +1,30 @@
-import {LobbyServer} from "./lobby_server_interface.ts";
+import { LobbyServer } from "./lobby_server_interface.ts";
 
 Deno.serve((req) => {
-  
+
     const lobbyServer = new LobbyServer();
 
     if (req.headers.get("upgrade") != "websocket") {
       return new Response(null, { status: 501 });
     }
-    
+
     const { socket, response } = Deno.upgradeWebSocket(req);
-    
-    socket.addEventListener("enter_lobby_from_front", (event) => {
 
-        console.log("Entered Client from Front Server");
+    socket.addEventListener("message", (event) => {
+        try {
+            // event.data를 JSON으로 파싱
+            const jsonData = JSON.parse(event.data);
+            console.log("Received JSON data:", jsonData);
 
-        // lobbyServer.NewUser()
-        // console.log("Recv data" + event.);
+            // 예시로 JSON에서 특정 필드 접근
+            if (jsonData.type === "enter_lobby_from_front") {
+                console.log("Client entered the lobby:", jsonData.user);
+            }
 
+        } catch (error) {
+            console.error("Failed to parse JSON:", error);
+        }
     });
-    
-    // socket.addEventListener("message", (event) => {
-    //   if (event.data === "ping") {
-    //     socket.send("pong");
-    //   }
-    // });
-    
-    return response;
 
+    return response;
 });
