be2887f45529b185d0c50de3c825f19cd1aba96b
kimmo <hemakim0215@naver.com>
2024-09-23 23:43:35 +0900

    .

--- ws_deno/shooting_game_server/lobby/lobby_server.ts
+++ ws_deno/shooting_game_server/lobby/lobby_server.ts
@@ -1,14 +1,21 @@
+import {LobbyServer} from "./lobby_server_interface.ts";
+
 Deno.serve((req) => {
-    
+  
+    const lobbyServer = new LobbyServer();
+
     if (req.headers.get("upgrade") != "websocket") {
       return new Response(null, { status: 501 });
     }
     
     const { socket, response } = Deno.upgradeWebSocket(req);
     
-    socket.addEventListener("enter_lobby_from_front", () => {
+    socket.addEventListener("enter_lobby_from_front", (event) => {
 
         console.log("Entered Client from Front Server");
+
+        // lobbyServer.NewUser()
+        // console.log("Recv data" + event.);
 
     });
     
