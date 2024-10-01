7e8cc0bfe2cc22d72b29fdb3df7b2ad51e919cf7
kimmo <hemakim0215@naver.com>
2024-10-01 11:59:08 +0900

    .

--- ws_deno/shooting_game_server/common/user.ts
+++ ws_deno/shooting_game_server/common/user.ts
@@ -29,5 +29,46 @@ export class User {
     ChangeConnectionType(changedType : UserConnectionType) {
         this.connection_type = changedType;
     }
-  
-  }
+
+    
+
+}
+
+// const address = { hostname: "127.0.0.1", port: 8080 };
+// const socket = Deno.listenDatagram("udp", address);
+
+// console.log(`UDP 서버가 ${address.hostname}:${address.port}에서 실행 중입니다.`);
+
+// for await (const [data, remote] of socket) {
+//   const message = new TextDecoder().decode(data);
+//   console.log(`클라이언트로부터 받은 메시지: ${message} (from ${remote.hostname}:${remote.port})`);
+
+//   // 받은 메시지를 클라이언트에게 그대로 돌려줌 (에코)
+//   const response = new TextEncoder().encode(message);
+//   socket.send(response, remote);
+// }
+
+
+
+// const address = { hostname: "127.0.0.1", port: 8080 };
+// const socket = Deno.listenDatagram("udp", address);
+// const clients = new Map<string, { hostname: string; port: number }>(); // 클라이언트 정보 저장
+
+// console.log(`UDP 서버가 ${address.hostname}:${address.port}에서 실행 중입니다.`);
+
+// for await (const [data, remote] of socket) {
+//   const message = new TextDecoder().decode(data);
+//   const clientKey = `${remote.hostname}:${remote.port}`;
+
+//   // 새로운 클라이언트인 경우, 클라이언트 목록에 추가
+//   if (!clients.has(clientKey)) {
+//     clients.set(clientKey, { hostname: remote.hostname, port: remote.port });
+//     console.log(`새 클라이언트 추가: ${clientKey}`);
+//   }
+
+//   console.log(`클라이언트로부터 받은 메시지: ${message} (from ${clientKey})`);
+
+//   // 받은 메시지를 클라이언트에게 에코
+//   const response = new TextEncoder().encode(message);
+//   socket.send(response, remote);
+// }
--- ws_deno/shooting_game_server/game/play_server_interface.ts
+++ ws_deno/shooting_game_server/game/play_server_interface.ts
@@ -2,17 +2,32 @@ import { Player } from "../common/game_data.ts";
 import { GamePakcetPing, GamePacketDamage, GamePacketTransformation } from "../common/game_packet.ts";
 import { MessageHandler } from "../common/game_message_action.ts";
 
+// Map<string, { hostname: string; port: number }>();
+
 export class GameServer {
+    private playerContainer : Map<string, Player>;
     private players: Player[];
     private messageHandler : MessageHandler;
 
     constructor() {
+        this.playerContainer = new Map<string, Player>();
         this.players = [];
         this.messageHandler = new MessageHandler();
     }
 
     newPlayer(player : Player) {        
         this.players.push(player);
+    }
+
+    isExistPlayer(key : string) : boolean {
+        if (!this.playerContainer.has(key)) {
+            return false;
+        }
+        return true;
+    }
+
+    addNewPlayer(key : string, player : Player) {
+        this.playerContainer.set(key, player);
     }
 
     messageAction(msg: string) {
